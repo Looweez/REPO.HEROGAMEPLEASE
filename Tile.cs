53f1034c3201f473f3cf56cd53493cfdbfc0b37f
Louise Lee <louiselee2005@gmail.com>
2024-09-05 10:20:12 +0200

    asdada

---
+++ Tile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEROGAMEPLEASE
+{
+    internal class Tile
+    {
+        //hdfgdfg
+        
+        
+            private int xPosition;
+            private int yPosition;
+            private char Display;
+
+            public int XPosition
+            {
+                get { return xPosition; }
+            }
+
+            public int YPosition
+            {
+                get { return yPosition; }
+            }
+
+            public Tile(int xPosition, int yPosition) // constructor
+            {
+
+                this.xPosition = xPosition;
+                this.yPosition = yPosition;
+            }
+            public char display
+            {
+                get { return Display; }
+            }
+        
+    }
+}
